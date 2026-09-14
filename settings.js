e6894722f4fdf3853b14db90065bba23116271b2
Midnight Cyber Rider <221291534+Dexsam07@users.noreply.github.com>
2026-09-14 13:14:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '.‎',
   botName: "Dexbotmd",
   botOwner: 'Dex Shyam Chaudhari', // Your name
-  ownerNumber: '917384287404', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
